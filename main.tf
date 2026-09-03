d7a4ed3751b1a98f942be1257402d15362da9025
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-09-03 16:58:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "demo" {
 bucket = "my-githubactions-bucket-oo99"
 }
 
+
 resource "aws_instance" "my_server" {
   ami           = "ami-081b0a6eac00b4f53" 
   instance_type = "t3.micro"             
